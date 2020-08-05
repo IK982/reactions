22686a4bf502992673509f06e318f98f1fcf6364
India <india.kaurbrown@zoopla.co.uk>
2020-08-05 15:26:45 +0100

    Part 2
    When you click on the active button, it randomly highlights the next active button.

--- src/Game/Button/Button.tsx
+++ src/Game/Button/Button.tsx
@@ -1,4 +1,4 @@
-﻿import React, {FunctionComponent} from "react";
+﻿import React, { FunctionComponent } from "react";
 import styles from "./Button.module.scss";
 
 interface ButtonProps {
@@ -6,22 +6,35 @@ interface ButtonProps {
     x: number;
     y: number;
     activeButtonId: number;
+    setActiveButtonId: (id: number) => void;
 }
 
-export const Button: FunctionComponent<ButtonProps> = ({id, x, y, activeButtonId}) => {
+export const Button: FunctionComponent<ButtonProps> = ({ id, x, y, activeButtonId, setActiveButtonId }) => {
 
-    
+
     const isActive = id === activeButtonId;
-    const getClassName = ():string => {
+    const getClassName = (): string => {
         if (isActive) {
             return `${styles.button} ${styles.active}`
-
         }
         return styles.button;
-
+    }
+    const buttonClicked = (): void => {
+        
+        
+        
+        if(isActive) {
+            const nextButton = Math.floor(Math.random() * 8);
+            setActiveButtonId(nextButton);
+        }
     }
     return (
-        <circle className={getClassName()} cx={x} cy={y} r={100}/>
+
+        <circle onClick={buttonClicked} className={getClassName()} cx={x} cy={y} r={100} />
 
     );
 };
+
+    // returns a random integer between 0 and the number of buttons.
+
+
