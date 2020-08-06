4e591f7ac7aac58f15e4f5d6b77b961d70fdeeb4
India <india.kaurbrown@zoopla.co.uk>
2020-08-06 09:29:01 +0100

    Part 3: Keeping score
    -Added state that keeps track of current score
    -Displayed the score in the h1
    -Every time you click an active button, the score increases by one

--- src/Game/Button/Button.tsx
+++ src/Game/Button/Button.tsx
@@ -1,4 +1,4 @@
-﻿import React, { FunctionComponent } from "react";
+﻿import React, { FunctionComponent, useState } from "react";
 import styles from "./Button.module.scss";
 
 interface ButtonProps {
@@ -7,9 +7,12 @@ interface ButtonProps {
     y: number;
     activeButtonId: number;
     setActiveButtonId: (id: number) => void;
+    currentScore: number;
+    setCurrentScore: (currentScore: number) => void;
+    
 }
 
-export const Button: FunctionComponent<ButtonProps> = ({ id, x, y, activeButtonId, setActiveButtonId }) => {
+export const Button: FunctionComponent<ButtonProps> = ({ id, x, y, activeButtonId, setActiveButtonId, currentScore, setCurrentScore }) => {
 
 
     const isActive = id === activeButtonId;
@@ -19,15 +22,16 @@ export const Button: FunctionComponent<ButtonProps> = ({ id, x, y, activeButtonI
         }
         return styles.button;
     }
+
     const buttonClicked = (): void => {
-        
-        
-        
         if(isActive) {
             const nextButton = Math.floor(Math.random() * 8);
             setActiveButtonId(nextButton);
+            setCurrentScore(currentScore + 1)
         }
     }
+
+  
     return (
 
         <circle onClick={buttonClicked} className={getClassName()} cx={x} cy={y} r={100} />
--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -1,11 +1,13 @@
-﻿import React, {FunctionComponent} from "react";
+﻿import React, {FunctionComponent, useState} from "react";
 import styles from "./Game.module.scss";
 import {Board} from "./Board/Board";
 
 export const Game: FunctionComponent = () => {
+    const [currentScore, setCurrentScore] = useState(0);
     return (
         <section className={styles.content}>
-            <Board/>
+            <h1>{currentScore}</h1>
+            <Board currentScore={currentScore} setCurrentScore={setCurrentScore}/>
         </section>
     );
 };
